b650eec12a914c562ed4f45748e44f84d8305bcb
SmartCrm <gruposmartdesenvolvimentos@gmail.com>
2024-09-02 22:20:51 -0300

    teste menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './_components/footer/footer.component';
+import { HeaderComponent } from './_components/header/header.component';
+import { SidenavComponent } from './_components/side-bar/side-bar.component';
+import { SublevelMenuComponent } from './_components/side-bar/sublevel-menu.component';
+import { BodyComponent } from './_components/body/body.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,    
+    SidenavComponent,
+    SublevelMenuComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
