b8180684671b1b46271e0950890d0c7ea73de3eb
dev.jhonatan.faelis <dev.jhonatan.faelis@gmail.com>
2024-11-27 23:27:58 +0000

    atualizacao

--- src/app/_module/planoModule.ts
+++ src/app/_module/planoModule.ts
@@ -1,28 +1,16 @@
 export interface Plano{
 
     Id: number;
-    Descricao: string;
-    TempoMinutos: number;
-    CentroCustoId?: number;
-    ValorPlano: number;
-    PlanoBimestral: boolean;
-    ValorMesBimestral?: number;
-    ValorTotalBimestral?: number;
-    DescontoMesBimestral?: number;
-    PlanoTrimestral: boolean;
-    ValorMesTrimestral?: number;
-    ValorTotalTrimestral?: number;
-    DescontoMesTrimestral?: number;
-    PlanoQuadrimestral: boolean;
-    ValorMesQuadrimestral?: number;
-    ValorTotalQuadrimestral?: number;
-    DescontoMesQuadrimestral?: number;
-    PlanoSemestral: boolean;
-    ValorMesSemestral?: number;
-    ValorTotalSemestral?: number;
-    DescontoMesSemestral?: number;
-    PlanoAnual: boolean;
-    ValorMesAnual?: number;
-    ValorTotalAnual?: number;
-    DescontoMesAnual?: number;
+    descricao: string;
+    tempoMinutos: number;
+    diasSemana: number;
+    centroCustoId?: number;
+    valorBimestral?: number;
+    valorTrimestral?: number;
+    valorQuadrimestral?: number;
+    valorSemestral?: number;
+    valorAnual?: number;
+    valorMensal?: number;
+    
+    
 }
