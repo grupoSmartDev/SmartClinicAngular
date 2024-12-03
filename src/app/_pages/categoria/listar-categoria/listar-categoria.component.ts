3caf44d01ff5710bf0a8470e40ea4434aec37472
dev.jhonatan.faelis <dev.jhonatan.faelis@gmail.com>
2024-12-03 20:38:22 +0000

    categorias

--- src/app/_pages/categoria/listar-categoria/listar-categoria.component.ts
+++ src/app/_pages/categoria/listar-categoria/listar-categoria.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { CategoriaService } from '../../../_services/categoria.service';
+import { ToastrService } from 'ngx-toastr';
+import { ModalCategoriaComponent } from '../modal-categoria/modal-categoria.component';
+import { ConfirmDialogComponent } from '../../../_components/confirm-dialog/confirm-dialog.component';
+import { Categoria } from '../../../_module/categoriaModule';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-listar-categoria',
@@ -6,5 +12,77 @@ import { Component } from '@angular/core';
   styleUrl: './listar-categoria.component.css'
 })
 export class ListarCategoriaComponent {
+  constructor(
+    private categoriaService: CategoriaService,
+    private toast: ToastrService) { }
 
+  @ViewChild(ModalCategoriaComponent) modal!: ModalCategoriaComponent;
+  @ViewChild('confirmDialog') confirmDialog!: ConfirmDialogComponent;
+  lista: Categoria[] = [];
+  errorMessage: string = '';
+  idParaExcluir!: string;
+  dadosParaExcluir!: Categoria;
+
+  ngOnInit(): void {
+    this.getDados();
+  }
+
+  atualizarLista(): void {
+    this.getDados(); // Chama o método para buscar os status novamente
+  }
+
+  getDados(): void {
+    this.categoriaService.Listar().subscribe({
+      next: (data) => {
+        if (data.dados) {
+          this.lista = data.dados;
+        }
+      },
+      error: (err) => {
+        console.error('Erro ao buscar categoria:', err);
+        this.errorMessage = 'Erro ao carregar os categoria. Tente novamente mais tarde.';
+      }
+    });
+  }
+
+  
+  openModal(categoria: any) {
+    debugger
+    if (categoria.id) {
+      this.modal.categoria = categoria;
+      this.modal.carregarDados(categoria);
+    }
+    const modalElement = document.getElementById('modalEditarCriar');
+    if (modalElement) {
+      const modal = new bootstrap.Modal(modalElement);
+      modal.show();
+    }
+  }
+
+  promptDelete(id: string) {
+    this.idParaExcluir = id;
+    this.confirmDialog.openDialog();
+  }
+
+  confirmDelete() {
+    this.Excluir(this.dadosParaExcluir);
+  }
+
+  cancelDelete() {
+    this.idParaExcluir = '';
+  }
+
+
+  Excluir(categoria : Categoria) {
+    let id = categoria.id;
+    this.categoriaService.Deletar(id).subscribe({
+      next: (response) => {
+        this.lista = this.lista.filter(categoria => categoria.id !== id);
+        this.toast.success('categoria excluido com sucesso!', 'Excluído');
+      },
+      error: () => {
+        this.toast.error('Tente novamente ou fale com o suporte', 'Erro ao excluir um categoria');
+      }
+    })
+  }
 }
--- src/app/_pages/categoria/modal-categoria/modal-categoria.component.ts
+++ src/app/_pages/categoria/modal-categoria/modal-categoria.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { CategoriaService } from '../../../_services/categoria.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { Categoria } from '../../../_module/categoriaModule';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ResponseModel } from '../../../_module/ResponseModule';
 
 @Component({
   selector: 'app-modal-categoria',
@@ -6,5 +12,61 @@ import { Component } from '@angular/core';
   styleUrl: './modal-categoria.component.css'
 })
 export class ModalCategoriaComponent {
+  constructor(
+    private categoriaService: CategoriaService,
+    private toast: ToastrService,
+    private router: Router) { }
 
+    @ViewChild('modalCategoria') modalCategoria?: ElementRef;
+    @Input() categoria = {} as Categoria;
+    @Input() nomeModal? : string;
+    @Output() DadosAtualizados = new EventEmitter<void>(); // Adicione este EventEmitter
+  
+    formulario = new FormGroup({
+      id: new FormControl(),
+      descricao: new FormControl()
+    });
+
+    onSubmit() {
+      const btnCacelar = document.querySelector('#btnCancelar') as HTMLElement;
+      if (this.formulario.valid) {
+        const dadosToSave: Categoria = this.formulario.value as Categoria;
+        if (dadosToSave.id) {
+          this.categoriaService.Atualizar(dadosToSave).subscribe({
+            next: (response: ResponseModel<Categoria>) => {
+              this.toast.success('Tipo de pagamento atualizado com Sucesso', 'Parabéns');
+              this.DadosAtualizados.emit(); // Emita o evento após a atualização
+              btnCacelar.click();
+              this.fecharModal();
+            },
+            error: (err) => {
+              this.toast.error('Tente novamente ou fale com o suporte', 'Erro ao atualizar um Tipo de pagamento');
+            }
+          });
+        } else {
+          this.categoriaService.Criar(dadosToSave).subscribe({
+            next: (response: ResponseModel<Categoria>) => {
+              this.toast.success('Tipo de pagamento Criado com sucesso', 'Parabéns');
+              this.DadosAtualizados.emit(); // Emita o evento após a criação
+              btnCacelar.click();
+              this.fecharModal();
+            },
+            error: (err) => {
+              console.error('Erro ao criar status:', err);
+              this.toast.error('Tente novamente ou fale com o suporte', 'Erro ao criar um Tipo de pagamento');
+            }
+          });
+        }
+      } else {
+        console.error('Formulário inválido');
+      }
+    }
+
+    carregarDados(categoria: any) {
+      this.formulario.patchValue(this.categoria);
+    }
+  
+    fecharModal() {
+      this.formulario.reset();
+    }
 }
