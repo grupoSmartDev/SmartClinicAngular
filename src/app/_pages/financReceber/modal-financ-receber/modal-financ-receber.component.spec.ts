f153c3f90afb78b974315b0ff3bfdf84f6842e16
BrunoBarrichello <bruno.barrichello@hotmail.com>
2024-12-05 21:41:14 -0300

    05/12/24 Bruno 21:41

--- src/app/_pages/financReceber/modal-financ-receber/modal-financ-receber.component.spec.ts
+++ src/app/_pages/financReceber/modal-financ-receber/modal-financ-receber.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ModalFinancReceberComponent } from './modal-financ-receber.component';
+import { ModalFinanceiroReceber  } from './modal-financ-receber.component';
 
 describe('ModalFinancReceberComponent', () => {
-  let component: ModalFinancReceberComponent;
-  let fixture: ComponentFixture<ModalFinancReceberComponent>;
+  let component: ModalFinanceiroReceber ;
+  let fixture: ComponentFixture<ModalFinanceiroReceber>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ModalFinancReceberComponent]
+      declarations: [ModalFinanceiroReceber]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ModalFinancReceberComponent);
+    fixture = TestBed.createComponent(ModalFinanceiroReceber);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
