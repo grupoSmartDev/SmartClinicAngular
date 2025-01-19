1cc3834d0ed76e0ecc6e0c161799e199b0d989e0
Jhonatan <dev.jhonatan.faelis@gmail.com>
2025-01-19 11:11:52 -0300

    att

--- src/app/_services/ficha-avaliacao.service.ts
+++ src/app/_services/ficha-avaliacao.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { FichaAvaliacao } from '../_module/fichaAvaliacaoModule';
+import { ResponseModel } from '../_module/ResponseModule';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FichaAvaliacaoService {
 
-  constructor() { }
+   constructor(private http: HttpClient) { }
+  
+    baseURL: string = 'https://localhost:44308/api/fichaAvaliacao/';
+
+    BuscarId(pacienteId : string): Observable<ResponseModel<FichaAvaliacao>>{
+      let params = new HttpParams();
+
+      params = params.set('pacienteId', pacienteId)
+      return this.http.get<ResponseModel<FichaAvaliacao>>(`${this.baseURL}BuscarPorPaciente`, {params})
+    }
+
+    Criar(ficha: FichaAvaliacao): Observable<ResponseModel<FichaAvaliacao>> {
+      return this.http.post<ResponseModel<FichaAvaliacao>>(`${this.baseURL}Criar`, ficha);
+    }
+  
+    Atualizar(ficha: FichaAvaliacao): Observable<ResponseModel<FichaAvaliacao>> {
+      return this.http.put<ResponseModel<FichaAvaliacao>>(`${this.baseURL}Editar`, ficha);
+    }
 }
