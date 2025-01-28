9d632ba95bf3f76e52ed3bc5e83cb471a3bbfe52
BrunoBarrichello <bruno.barrichello@hotmail.com>
2025-01-28 19:29:48 -0300

    28/01/25 Bruno 19:29 - Ajustes Environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // environment.prod.ts (produção)
 export const environment = {
     production: true,
-    apiUrl: 'https://clinicsmartbackend.onrender.com/' // sua URL de produção
+    apiUrl: 'https://webapismartclinic-x9es.onrender.com' // sua URL de produção
   };
