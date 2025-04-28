dbf56ab00a78b1b7e9180ff3c7dbf232f6aeeaca
Jhonatan <dev.jhonatan.faelis@gmail.com>
2025-04-28 17:04:50 -0300

    cache sala

--- src/app/_pages/sala/listar-salas/listar-salas.component.ts
+++ src/app/_pages/sala/listar-salas/listar-salas.component.ts
@@ -6,14 +6,20 @@ import { Sala } from '../../../_module/salasModule';
 import { SalasService } from '../../../_services/salas.service';
 import { ToastrService } from 'ngx-toastr';
 import * as bootstrap from 'bootstrap';
+import { TabService } from '../../../_services/tabs.service';
+
+interface CacheData {
+  cacheList: Sala[];
+  totalItems: number;
+  timestamp: number;
+}
 
 @Component({
   selector: 'app-listar-salas',
   templateUrl: './listar-salas.component.html',
-  styleUrl: './listar-salas.component.css'
+  styleUrl: './listar-salas.component.css',
 })
 export class ListarSalasComponent {
-
   @ViewChild(ModalSalasComponent) modalSalaComponent!: ModalSalasComponent;
   @ViewChild('confirmDialog') confirmDialog!: ConfirmDialogComponent;
   lista: Sala[] = [];
@@ -21,38 +27,84 @@ export class ListarSalasComponent {
   idParaExcluir!: string;
   salaParaExcluir!: Sala;
   mostrarFiltros: boolean = true; // Começa expandido por padrão
-    //paginacao
-    totalItems: number = 0;
-    pageSize: number = 10;
-    currentPage: number = 1;
-    // filtros
-    nomeFiltro: string = '';
-    idFiltro: string = '';
-    localFiltro: string = '';
-    capacidadeFiltro: string = '';
-    paginar : boolean = true;
+  //paginacao
+  totalItems: number = 0;
+  pageSize: number = 10;
+  currentPage: number = 1;
+  // filtros
+  nomeFiltro: string = '';
+  idFiltro: string = '';
+  localFiltro: string = '';
+  capacidadeFiltro: string = '';
+  paginar: boolean = true;
 
-  constructor(private salaService: SalasService , private toast: ToastrService) { }
+  private readonly CACHE_DURATION = 5 * 60 * 1000; // 5 minutos em milissegundos
+  cacheList: Sala[] = [];
+
+  constructor(
+    private salaService: SalasService,
+    private toast: ToastrService,
+    private tabService: TabService
+  ) {}
+
+  private getCacheKey(): string {
+    // Cria uma chave única para o cache baseada nos parâmetros atuais
+    return `sala-list-${this.currentPage}-${this.pageSize}-${this.cacheList}`;
+  }
+
+  private isCacheValid(timestamp: number): boolean {
+    return Date.now() - timestamp < this.CACHE_DURATION;
+  }
+
+  // Método para invalidar o cache quando necessário
+  private invalidateCache(): void {
+    const cacheKey = this.getCacheKey();
+    this.tabService.setCacheData(cacheKey, null);
+  }
 
   ngOnInit(): void {
     this.loadData();
-  } 
+  }
 
   loadData(): void {
-    this.salaService.Listar(this.currentPage,this.pageSize,this.nomeFiltro,
-      this.idFiltro,this.localFiltro,this.capacidadeFiltro,
-      this.paginar).subscribe({
-      next: (data) => {
-        if (data.dados) {
-          this.lista = data.dados;
-          this.totalItems = data.totalCount ?? 0;
-        }
-      },
-      error: (err) => {
-        console.error('Erro ao buscar Sala:', err);
-        this.errorMessage = 'Erro ao carregar as salas. Tente novamente mais tarde.';
-      }
-    });
+    const cacheKey = this.getCacheKey();
+    const cachedData = this.tabService.getCacheData(cacheKey) as CacheData;
+
+    if (cachedData && this.isCacheValid(cachedData.timestamp)) {
+      // Se temos dados em cache válidos, use-os
+      this.lista = cachedData.cacheList;
+      this.totalItems = cachedData.totalItems;
+    } else {
+      this.salaService
+        .Listar(
+          this.currentPage,
+          this.pageSize,
+          this.nomeFiltro,
+          this.idFiltro,
+          this.localFiltro,
+          this.capacidadeFiltro,
+          this.paginar
+        )
+        .subscribe({
+          next: (data) => {
+            if (data.dados) {
+              this.lista = data.dados;
+              this.totalItems = data.totalCount ?? 0;
+
+              this.tabService.setCacheData(cacheKey, {
+                cacheList: this.cacheList,
+                totalItems: this.totalItems,
+                timestamp: Date.now(),
+              });
+            }
+          },
+          error: (err) => {
+            console.error('Erro ao buscar Sala:', err);
+            this.errorMessage =
+              'Erro ao carregar as salas. Tente novamente mais tarde.';
+          },
+        });
+    }
   }
 
   openModal(sala: any) {
@@ -67,26 +119,31 @@ export class ListarSalasComponent {
     }
   }
 
-  Excluir(sala : Sala) {
+  Excluir(sala: Sala) {
     let id = sala.id;
     this.salaService.Deletar(parseInt(id)).subscribe({
       next: (response) => {
         console.log('Sala excluído com sucesso:', response);
-        this.lista = this.lista.filter(sala => sala.id !== id);
+        this.lista = this.lista.filter((sala) => sala.id !== id);
         this.toast.success('Sala excluído com sucesso!', 'Excluído');
+
+        this.invalidateCache();
       },
       error: (err) => {
         console.error('Erro ao excluir sala:', err);
-        this.toast.error('Tente novamente ou fale com o suporte', 'Erro ao excluir uma sala');
-      }
+        this.toast.error(
+          'Tente novamente ou fale com o suporte',
+          'Erro ao excluir uma sala'
+        );
+      },
     });
   }
-  
+
   atualizarLista(): void {
     this.loadData(); // Chama o método para buscar os status novamente
   }
 
-  promptDelete(dataParaExcluir : any) {
+  promptDelete(dataParaExcluir: any) {
     this.salaParaExcluir = dataParaExcluir;
     this.confirmDialog.openDialog();
   }
@@ -105,22 +162,22 @@ export class ListarSalasComponent {
   }
 
   onSearch(): void {
+    this.invalidateCache();
     this.currentPage = 1;
     this.loadData();
   }
 
+  toggleFiltros() {
+    this.mostrarFiltros = !this.mostrarFiltros;
+  }
 
-
-toggleFiltros() {
-  this.mostrarFiltros = !this.mostrarFiltros;
-}
-
-limparFiltros() {
-  this.nomeFiltro = '';
-  this.idFiltro = '';
-  this.localFiltro = '';
-  this.capacidadeFiltro = '';
-  // Opcional: realizar uma busca após limpar
-  this.onSearch();
-}
+  limparFiltros() {
+    this.invalidateCache();
+    this.nomeFiltro = '';
+    this.idFiltro = '';
+    this.localFiltro = '';
+    this.capacidadeFiltro = '';
+    // Opcional: realizar uma busca após limpar
+    this.onSearch();
+  }
 }
