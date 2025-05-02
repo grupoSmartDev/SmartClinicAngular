34c6e2b5dabe5a3356f58a65841767189c6c2838
BrunoBarrichello <bruno.barrichello@hotmail.com>
2025-05-02 19:27:00 -0300

    Update salasModule.ts

--- src/app/_module/salasModule.ts
+++ src/app/_module/salasModule.ts
@@ -4,7 +4,7 @@ export interface Sala{
     capacidade: number;
     tipo: string;
     local: string;
-    status: string;
+    status: boolean;
     observacao: string;
     horarioFincionamento : string;
 }
