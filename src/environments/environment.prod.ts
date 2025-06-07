b595bcaf63698be15a48460f9dc6fec3ea949b44
grupoSmartDev <gruposmartdesenvolvimentos@gmail.com>
2025-06-07 14:51:03 -0300

    07/06/25 Bruno 14:50

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 export const environment = {
     production: true,
     // apiUrl: 'https://webapismartclinic-x9es.onrender.com/' // sua URL de produção.
-    apiUrl: 'http://62.72.51.219:3332/' // sua URL de produção.
+    apiUrl: 'https://api.clinicsmart.app.br/' // sua URL de produção.
   };
--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 // environment.prod.ts (produção)
 export const environment = {
     production: true,
-    apiUrl: 'http://62.72.51.219:3332/' // sua URL de produção.
+    apiUrl: 'https://api.clinicsmart.app.br/' // sua URL de produção.
 };
