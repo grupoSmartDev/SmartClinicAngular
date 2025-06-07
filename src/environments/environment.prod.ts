d25cd6a5154cabadeee2f80a035146d24ff3fbc3
grupoSmartDev <gruposmartdesenvolvimentos@gmail.com>
2025-06-07 07:27:39 -0300

    ip vps hostinger

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 // environment.prod.ts (produção)
 export const environment = {
     production: true,
-    apiUrl: 'https://webapismartclinic-x9es.onrender.com/' // sua URL de produção.
+    // apiUrl: 'https://webapismartclinic-x9es.onrender.com/' // sua URL de produção.
+    apiUrl: 'http://62.72.51.219:3332/' // sua URL de produção.
   };
