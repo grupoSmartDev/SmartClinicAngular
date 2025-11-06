bd7c52a44f32f4942278607dbbba541f641ade48
BrunoBarrichello <99931716+BrunoBarrichello@users.noreply.github.com>
2025-11-06 18:43:31 -0300

    06/11/25 Bruno 18:43 - Deletado arquivos não utilizados de teste

--- src/app/_shared/pipes/currency-pt-br.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CurrencyPtBrPipe } from './currency-pt-br.pipe';
-
-describe('CurrencyPtBrPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CurrencyPtBrPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/_shared/pipes/telefone-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TelefonePipePipe } from './telefone-pipe.pipe';
-
-describe('TelefonePipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new TelefonePipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
